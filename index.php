6140081d9c11cf6681bec1a40a8858b828d1c90a
Jeff Linson <jlinson@coldspringsoftware.com>
2013-09-17 14:27:54 -0400

    Added base index.php page.

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+		<title>Welcome to P1</title>
+		<meta name="description" content="" />
+		<meta name="author" content="Jeff Linson" />
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="/favicon.ico" />
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	</head>
+
+	<body>
+		<div>
+			<header>
+				<h1> <?php echo '<p>Welcome to P1</p>'; ?> </h1>
+			</header>
+			<nav>
+				<p>
+				</p>
+			</nav>
+
+			<div>
+				<p>(PHP version)</p>
+			</div>
+
+			<footer>
+				<p>
+					&copy; Copyright 2013 by Jeff Linson
+				</p>
+			</footer>
+		</div>
+	</body>
+</html>
