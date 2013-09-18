670cb9cebb017e9dba767a2c427e2ec3fdf6dfac
Jeff Linson <jlinson@coldspringsoftware.com>
2013-09-17 20:51:08 -0400

    Major update of site with full page and css.

--- index.php
+++ index.php
@@ -1,42 +1,144 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8" />
-
-		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-		Remove this if you use the .htaccess -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
-		<title>Welcome to P1</title>
-		<meta name="description" content="" />
-		<meta name="author" content="Jeff Linson" />
-
-		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
-
-		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
-		<link rel="shortcut icon" href="/favicon.ico" />
-		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-	</head>
-
-	<body>
-		<div>
-			<header>
-				<h1> <?php echo '<p>Welcome to P1</p>'; ?> </h1>
-			</header>
-			<nav>
-				<p>
-				</p>
-			</nav>
-
-			<div>
-				<p>(PHP version - chmod fix test)</p>
-			</div>
-
-			<footer>
-				<p>
-					&copy; Copyright 2013 by Jeff Linson
-				</p>
-			</footer>
-		</div>
-	</body>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="description" content="Project 1, P1, DWA E-15">
+    <meta name="author" content="Jeff Linson using CoffeeCup Software, Inc.">
+    <meta name="Copyright" content="Copyright (c) 2010 CoffeeCup, all rights reserved.">
+	<!-- Coffee Cup Basic Gray Theme used as starting point. -->
+	<title>Welcome to P1 | Jeffrey Linson</title>
+    <link rel="stylesheet" href="css/default.css">
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
+    <script type="text/javascript" src="js/behavior.js"></script>
+    <!--[if IE]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+	<header id="mast">
+    	<h1>Welcome to P1 for Jeffrey (Jeff) Linson</h1>
+    </header>
+    <nav id="global">
+    	<ul>
+        	<li><a class="selected" href="http://websandbox.biz">Parent Site</a></li>
+       <!-- <li><a href="about.html">About</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li><a href="contact.html">Contact</a></li>
+		- - - - - - - - - - - - - - - - - - - - - - - - - -->	
+        </ul>
+    </nav>
+    <section id="intro">
+    	<header>
+    		<h2>Basic Bio:</h2>
+        </header>
+        <p>After earning a B.S. in Chemistry from Yale, I spent ten years in engineering and business positions before settling into a career in business software development and project management.  
+		I've spent over ten years developing internal customer service, marketing and accounting applications for major corporations such as Pepsico, Pfizer, AIG and SeaLand (now part of Maersk).  Most
+		of that development involved C++, PowerBuilder frontends hitting Sybase or SQL Server databases running on Unix or Windows Servers.</p> 
+		<p>To add more formal training to my resume and expand my skills in the current web-centric world, I'm pursuing a masters degree in computer science through the Harvard Extension School.  I'm 
+		looking to grow a consulting business developing business CMS and ecommerce sites using open source tools such as Joomla and Magento Commerce.
+		
+        <!-- <a href="/">Read More!</a> -->
+        <div id="photo">
+        	<div>
+            	<!-- <h3>Photo</h3> -->
+				<img src="img/100_0003a.jpg" width="380" height="296" alt="Jeff (headshot)" title="Jeff" border="0" />
+            </div>
+        </div>
+    </section>
+    <div id="main" class="clear">
+        <section id="articles">
+            <article class="blogPost">
+                <header>
+                    <h2>Current Development Tools</h2>
+                    <!-- <p>Posted on <time pubdate datetime="2009-06-29T23:31:45-09:00">June 29th 2009</time> by <a href="#">Joe Blow</a> - <a href="#comments">3 comments</a></p> -->
+                </header>
+                    <ul>
+                        <li>Basic Text Editor: <a href="http://notepad-plus-plus.org/">Notepad ++</a> - comparable to Sublime Text; hugely popular; frequently updated.</li>
+                        <li>HTML Editor (trial): <a href="http://www.coffeecup.com/">CoffeeCup</a> - used for this project; more current than my old copy of Dreamweaver.</li>
+						<li>HTML Editor (dated): <a href="http://www.adobe.com/products/creativecloud.html">Dreamweaver MX Studio</a> - not updated due to expense; now all in the Cloud.</li>
+                        <li>PHP Editor: <a href="http://www.codelobster.com/">CodeLobster</a> - works nicely with Joomla, Yii Framework and JQuery.</li>
+                        <li>Project IDE: <a href="http://www.eclipse.org/">Eclipse with Aptana Studio plugin</a> - still wrestling with all the features; supports Git.</li>
+                        <li>HTML Inspector/Validator: <a href="https://addons.mozilla.org/en-us/firefox/addon/firebug/">Firebug &amp; HTML Tidy for Firefox</a> - really sheds light on dark places.</li>
+                    </ul><br>
+                <p>Outside of corporate environments, all my web development has been on Linux web hosts (typical LAMP stacks).  Previously I used XAMPP for a localhost.  As part of this class I've installed
+				   WAMP for comparison.  While I've used version control in the past (e.g. PVCS, SCCS and PowerBuilder Native), I'm new to Git.</p>
+            </article>
+            <article class="blogPost">
+                <header>
+                    <h2>Hobbies and Miscellaneous</h2>
+                    <!-- <p>Posted on <time pubdate datetime="2009-06-29T23:31:45-09:00">June 29th 2009</time> by <a href="#">Joe Blow</a> - <a href="#comments">3 comments</a></p> -->
+                </header>
+                <p>Outside of my interests in science and technology, I try to spend as much time as possible outdoors either on a bike or on skis.</p>
+                <p>I'm currently living in Cold Spring, New York.  While that is outside the New England states, I'm not that far from the Connecticut border.  I hope to venture up to Cambridge 
+				to attend some of the lectures in person.  However, the online (Live!) access to the lectures and class materials is extremely convenient.  Rather different than when I was an undergrad.</p>
+            </article>
+        </section>
+       <!-- Nice design, but not for this page at this time. jbl
+        <aside>
+            <section>
+                <header>
+                    <h3>Categories</h3>
+                </header>
+                <nav id="categories">
+                    <ul>
+                        <li><a href="/">Page Link</a></li>
+                        <li><a href="/">Page Link</a></li>
+                        <li><a href="/">Page Link</a></li>
+                        <li><a href="/">Page Link</a></li>
+                        <li><a href="/">Page Link</a></li>
+                        <li><a href="/">Page Link</a></li>
+                    </ul>
+                </nav>
+            </section>
+            <section>
+                <header>
+                    <h3>Archives</h3>
+                </header>
+                <nav id="archives">
+                    <ul>
+                        <li><a href="/">Page Link</a></li>
+                        <li><a href="/">Page Link</a></li>
+                        <li><a href="/">Page Link</a></li>
+                        <li><a href="/">Page Link</a></li>
+                        <li><a href="/">Page Link</a></li>
+                        <li><a href="/">Page Link</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </aside>
+		- killed the entire "aside" section for the moment. - jbl -->
+    </div>
+    <footer>
+    	<div class="clear">
+            <section id="about">
+                <header>
+                    <h3>About</h3>
+                </header>
+                <p>This page is presented as evidence of completion of Project 1 : Workflow setup and essential HTML5/CSS demonstration for Harvard Extension School class DWA E-15 Dynamic Web Applications.<br>
+				<a href="/">top</a></p>
+            </section>
+            <section>
+                <header>
+                    <h3>W3C Validation Link</h3>
+                </header>
+                <nav id="blogRoll">
+                    <ul>
+                        <li><a href="http://validator.w3.org/">W3C</a></li>
+                    </ul>
+                </nav>
+            </section>
+            <section>
+                <header>
+                    <h3>Site Map</h3>
+                </header>
+                <nav id="siteMap">
+                    <ul>
+                        <li><a href="http://websandbox.biz">Parent Site</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </div>
+    </footer>
+</body>
 </html>
